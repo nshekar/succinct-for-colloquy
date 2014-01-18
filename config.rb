828860cd9d0355b0e35ad2e3992ea8f16c684234
JohnAlbin <virtually.johnalbin@gmail.com>
2014-01-18 13:04:01 +0800

    Update config.rb file for Sass.

--- config.rb
+++ config.rb
@@ -1,20 +1,8 @@
 # First, require any additional compass plugins installed on your system.
 # require 'zen-grids'
-# require 'breakpoint'
 
 
-# Toggle this between :development and :production when deploying the CSS to the
-# live server. Development mode will retain comments and spacing from the
-# original Sass source and adds line numbering comments for easier debugging.
-environment = :production
-# environment = :development
-
-# In development, we can turn on the FireSass-compatible debug_info.
-firesass = false
-# firesass = true
-
-
-# Location of the your project's resources.
+# Location of the this project's resources.
 css_dir         = "Succinct.colloquyStyle/Contents/Resources"
 sass_dir        = "Succinct.colloquyStyle/Contents/Resources/sass"
 extensions_dir  = "Succinct.colloquyStyle/Contents/Resources/sass-extensions"
@@ -28,18 +16,9 @@ http_path = "/"
 # To use relative paths to assets in your compiled CSS files, set this to true.
 relative_assets = true
 
-
-##
-## You probably don't need to edit anything below this.
-##
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+output_style = :expanded
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
-# Pass options to sass. For development, we turn on the FireSass-compatible
-# debug_info if the firesass config variable above is true.
-sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
